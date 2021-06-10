3f907b042bef95e15b2aeca850ce5057ee51d02e
Maove <maove_1125@hotmail.com>
2021-06-09 22:11:18 -0500

    Modificación Layout

--- source/view/VentanaAgregarCita.java
+++ source/view/VentanaAgregarCita.java
@@ -7,16 +7,34 @@ import java.awt.*;
 
 public class VentanaAgregarCita extends JFrame
 {
+    /**
+     *
+     */
     private JLabel labPaciente, labFecha, labHora;
 
+    /**
+     *
+     */
     private JComboBox pacientes;
 
+    /**
+     *
+     */
     private JCalendar fecha;
 
+    /**
+     *
+     */
     private JComboBox hora;
 
+    /**
+     *
+     */
     private JButton btnAgregar, btnCancelar;
 
+    /**
+     *
+     */
     public VentanaAgregarCita()
     {
         setTitle("Nueva Cita");
@@ -35,7 +53,7 @@ public class VentanaAgregarCita extends JFrame
 
         pacientes = new JComboBox();
         c.fill = GridBagConstraints.HORIZONTAL;
-        //c.ipadx = -100;
+        //c.insets = new Insets(0,-10,0,0);
         c.gridx = 1;
         c.gridy = 0;
         add(pacientes,c);
@@ -43,62 +61,68 @@ public class VentanaAgregarCita extends JFrame
         labFecha = new JLabel("Fecha: ");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         add(labFecha,c);
 
         fecha = new JCalendar();
         c.fill = GridBagConstraints.HORIZONTAL;
         //c.ipadx = 40;
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         add(fecha,c);
 
         labHora = new JLabel("Hora: ");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 1;
         add(labHora,c);
 
         hora = new JComboBox();
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
-        c.gridy = 3;
+        c.gridy = 1;
         add(hora,c);
-
-        /*
-        JLabel labVacio1 = new JLabel();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 5;
-        add(labVacio1,c);
-
-        JLabel labVacio2 = new JLabel();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 1;
-        c.gridy = 5;
-        add(labVacio2,c);
-        */
 
         btnAgregar = new JButton("Agregar");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
-        c.gridy = 4;
+        c.gridy = 5;
         add(btnAgregar,c);
 
         btnCancelar = new JButton("Cancelar");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
-        c.gridy = 4;
+        c.gridy = 5;
         add(btnCancelar,c);
     }
 
+    /**
+     *
+     * @return
+     */
     public JComboBox getComboPacientes() { return pacientes; }
 
+    /**
+     *
+     * @return
+     */
     public JCalendar getFecha() { return fecha; }
 
+    /**
+     *
+     * @return
+     */
     public JComboBox getHora() { return hora; }
 
+    /**
+     *
+     * @return
+     */
     public JButton getBtnAgregar() { return btnAgregar; }
 
+    /**
+     *
+     * @return
+     */
     public JButton getBtnCancelar() { return btnCancelar; }
 }
