4717d4b89b42cd07f5c6e43ada4e884330613c9e
Maove <maove_1125@hotmail.com>
2021-06-09 22:44:34 -0500

    Modificación Layout

--- source/view/VentanaAgregarCita.java
+++ source/view/VentanaAgregarCita.java
@@ -3,6 +3,7 @@ package view;
 import com.toedter.calendar.JCalendar;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class VentanaAgregarCita extends JFrame
@@ -38,17 +39,47 @@ public class VentanaAgregarCita extends JFrame
     public VentanaAgregarCita()
     {
         setTitle("Nueva Cita");
-        setSize(300,300);
+        setSize(400,300);
         setResizable(false);
         setLocationRelativeTo(null);
 
-        setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        setLayout(new BorderLayout());
+
+        JPanel panelAux1 = new JPanel();
+        panelAux1.setLayout(new GridLayout(2,2));
+        labPaciente = new JLabel("Paciente: ");
+        panelAux1.add(labPaciente);
+        pacientes = new JComboBox();
+        pacientes.setSize(new Dimension(200,0));
+        panelAux1.add(pacientes);
+        labHora = new JLabel("Hora: ");
+        panelAux1.add(labHora);
+        hora = new JComboBox();
+        panelAux1.add(hora);
+
+        add(panelAux1, BorderLayout.NORTH);
+
+        fecha = new JCalendar();
+        add(fecha, BorderLayout.CENTER);
+
+        JPanel panelAux2 = new JPanel();
+        panelAux2.setLayout(new GridLayout(1,2));
+        btnAgregar = new JButton("Agregar");
+        panelAux2.add(btnAgregar);
+        btnCancelar = new JButton("Cancelar");
+        panelAux2.add(btnCancelar);
+
+        add(panelAux2, BorderLayout.SOUTH);
+
+
+        //setLayout(new GridBagLayout());
+        /*GridBagConstraints c = new GridBagConstraints();
 
         labPaciente = new JLabel("Paciente: ");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 0;
+        c.gridwidth = 1;
         add(labPaciente,c);
 
         pacientes = new JComboBox();
@@ -56,12 +87,14 @@ public class VentanaAgregarCita extends JFrame
         //c.insets = new Insets(0,-10,0,0);
         c.gridx = 1;
         c.gridy = 0;
+        c.gridwidth = 2;
         add(pacientes,c);
 
         labFecha = new JLabel("Fecha: ");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 2;
+        c.gridwidth = 1;
         add(labFecha,c);
 
         fecha = new JCalendar();
@@ -69,31 +102,38 @@ public class VentanaAgregarCita extends JFrame
         //c.ipadx = 40;
         c.gridx = 0;
         c.gridy = 3;
+        c.gridwidth = 1;
         add(fecha,c);
 
         labHora = new JLabel("Hora: ");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 1;
+        c.gridwidth = 1;
         add(labHora,c);
 
         hora = new JComboBox();
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 1;
+        c.gridwidth = 2;
         add(hora,c);
 
         btnAgregar = new JButton("Agregar");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 5;
+        c.gridwidth = 1;
         add(btnAgregar,c);
 
         btnCancelar = new JButton("Cancelar");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 5;
+        c.gridwidth = 1;
         add(btnCancelar,c);
+
+         */
     }
 
     /**
